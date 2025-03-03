27732065d1b2a8771fc4fd90748feb8ee78cead1
Hp <Hp@DESKTOP-407CTND>
2025-03-03 23:19:20 +0530

    initial commit files

---
+++ EFLearn.Basics/Models/FighterPlane.cs
@@ -0,0 +1,10 @@
+﻿namespace EFLearn.Basics.Models
+{
+    public class FighterPlane
+    {
+        public int Id { get; set; }
+        public  string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ EFLearn.DataWiz/LearningContext.cs
@@ -0,0 +1,18 @@
+﻿using EFLearn.Basics.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFLearn.DataWiz
+{
+    public class LearningContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FighterPlane>().Property(b => b.Name).IsRequired();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("server:(localdb)\\\\mssqllocaldb; database:Test_Prac ;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+        public DbSet<FighterPlane> FighterPlanes { get; set; }
+    }
+}
